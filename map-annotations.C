e8613e68d2271ebe3240b24c0c8748d9b2e50a91
Bill Majoros <martiandna@gmail.com>
2016-05-11 15:10:37 -0400

    update

--- map-annotations.C
+++ map-annotations.C
@@ -18,10 +18,15 @@ class Application {
 public:
   int main(int argc,char *argv[]);
 private:
+  bool optS;
+  String newSubstrate;
   CigarAlignment *alignment;
   void loadAlignment(const String &filename);
-  void mapTranscript(const GffTranscript &,ostream &os);
+  void mapTranscript(const GffTranscript &,ostream &);
   void mapExon(GffExon &);
+  void mapFeature(GffFeature &,ostream &);
+  void projectGenes(const String &filename,ostream &);
+  void projectNongenic(const String &filename,ostream &);
 };
 
 
@@ -55,15 +60,44 @@ map-annotations [options] <ref.gff> <ref-to-alt.cigar> <out.gff>\n\
   const String refGffFile=cmd.arg(0);
   const String cigarFile=cmd.arg(1);
   const String outGff=cmd.arg(2);
-  bool optS=cmd.option('s');
-  String newSubstrate; if(optS) newSubstrate=cmd.optParm('s');
+  optS=cmd.option('s');
+  if(optS) newSubstrate=cmd.optParm('s');
 
   // Load alignment (CIGAR)
   loadAlignment(cigarFile);
 
   // Project genes, if any
   ofstream os(outGff.c_str());
-  GffReader reader(refGffFile);
+  projectGenes(refGffFile,os);
+
+  // Project non-genic elements, if any
+  projectNongenic(refGffFile,os);
+
+  return 0;
+}
+
+
+
+void Application::projectNongenic(const String &filename,ostream &os)
+{
+  GffReader reader(filename);
+  Vector<GffFeature*> &features=*reader.loadFeatures();
+  for(Vector<GffFeature*>::iterator cur=features.begin(), end=
+	features.end() ; cur!=end ; ++cur) {
+    GffFeature *feature=*cur;
+    if(optS) feature->setSubstrate(newSubstrate);
+    mapFeature(*feature,os);
+    delete feature;
+  }
+  reader.close();
+  delete &features;
+}
+
+
+
+void Application::projectGenes(const String &filename,ostream &os)
+{
+  GffReader reader(filename);
   Vector<GffTranscript*> &transcripts=*reader.loadTranscripts();
   for(Vector<GffTranscript*>::iterator cur=transcripts.begin(), end=
 	transcripts.end() ; cur!=end ; ++cur) {
@@ -72,12 +106,8 @@ map-annotations [options] <ref.gff> <ref-to-alt.cigar> <out.gff>\n\
     mapTranscript(*transcript,os);
     delete transcript;
   }
+  reader.close();
   delete &transcripts;
-
-  // Project non-genic elements, if any
-
-
-  return 0;
 }
 
 
@@ -94,15 +124,25 @@ void Application::loadAlignment(const String &filename)
 
 
 
+void Application::mapFeature(GffFeature &feature,ostream &os)
+{
+  int begin=feature.getBegin(), end=feature.getEnd();
+  begin=alignment->mapApproximate(begin,DIR_NONE);
+  end=alignment->mapApproximate(end,DIR_NONE);
+  feature.setBegin(begin);
+  feature.setEnd(end);
+}
+
+
+
 void Application::mapExon(GffExon &exon)
 {
-  CigarAlignment &align=*alignment;
   int begin=exon.getBegin(), end=exon.getEnd();
 
   // These two lines map the splice sites across the
   // alignment, then use that to set exon boundaries:
-  begin=align.mapApproximate(begin-2,DIR_NONE)+2;
-  end=align.mapApproximate(end+1,DIR_NONE)-1;
+  begin=alignment->mapApproximate(begin-2,DIR_NONE)+2;
+  end=alignment->mapApproximate(end+1,DIR_NONE)-1;
   exon.setBegin(begin); exon.setEnd(end);
 }
 
