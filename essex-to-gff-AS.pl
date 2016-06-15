7608d01f8563dddc770543f38327a92b508b149d
Bill Majoros <martiandna@gmail.com>
2016-06-15 15:38:34 -0400

    update

---
+++ essex-to-gff-AS.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+use strict;
+use EssexParser;
+use EssexFBI;
+use ProgramName;
+$|=1;
+
+my $name=ProgramName::get();
+die "$name <in.essex> <out.gff> <allele#>\n" unless @ARGV==3;
+my ($infile,$outfile,$hap)=@ARGV;
+
+open(OUT,">$outfile") || die "can't write to file: $outfile\n";
+my $parser=new EssexParser($infile);
+while(1) {
+  my $root=$parser->nextElem();
+  last unless $root;
+  my $fbi=new EssexFBI($root);
+  my $transcriptID=$fbi->getTranscriptID();
+  my $status=$fbi->getStatusString();
+  if($status eq "mapped") {
+    my $transcript=$fbi->getMappedTranscript();
+    my $id=$transcript->getTranscriptId();
+    $id.="_$hap";
+    $transcript->setTranscriptId($id);
+    $id=$transcript->getGeneId();
+    $id.="_$hap";
+    $transcript->setGeneId($id);
+    print OUT $transcript->toGff();
+  }
+  elsif($status eq "splicing-changes") {
+    my $transcripts=$fbi->getAltTranscripts();
+    my $n=@$transcripts;
+    for(my $i=0 ; $i<$n ; ++$i) {
+      my $transcript=$transcripts->[$i];
+      my $id=$transcript->getTranscriptId();
+      $id="ALT$i\_$id\_$hap";
+      $transcript->setTranscriptId($id);
+      $id=$transcript->getGeneId();
+      $id.="_$hap";
+      $transcript->setGeneId($id);
+      print OUT $transcript->toGff();
+    }
+  }
+  undef $root; undef $fbi;
+}
+close(OUT);
+
+
